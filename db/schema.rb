edbf08147d1edcdd11108ffcfbb2d77e77971f47
agrzelewska <agatagrzelewska@gmail.com>
2019-02-05 20:25:59 +0100

    Ammending css - header_inner/nav

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180502185901) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
